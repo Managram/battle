a9a4b473e91617e0c71ded5fce92f2c6d2ee62d9
Patrick <p_donati@hotmail.com>
2016-03-18 19:43:45 +0000

    things

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature 'attack player 2' do
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
-    click_button "Attack player 2"
+    click_button "Attack"
 
-    expect(page).to have_text("player 2 was attacked")
+    expect(page).to have_text("ass attacked boob")
   end
 end
